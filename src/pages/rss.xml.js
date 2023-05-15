764eaade479f20766fde53fe1ad937d59147eabb
Jordan Bergero <jordan.bergero6@gmail.com>
2023-05-14 22:47:28 -0700

    astro blog tutorial

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,18 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+
+export async function get() {
+  const posts = await getCollection('blog');
+  return rss({
+    title: 'Astro Learner | Blog',
+    description: 'My journey learning Astro',
+    site: 'https://soft-raindrop-bf7931.netlify.app/',
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/blog/${post.slug}/`,
+    })),
+    customData: `<language>en-us</language>`,
+  });
+}
---
+++ src/scripts/menu.js
@@ -0,0 +1,4 @@
+document.querySelector('.hamburger').addEventListener('click', () => {
+    console.log('uh hi');
+    document.querySelector('.nav-links').classList.toggle('expanded');
+  });
