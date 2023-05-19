7d4ce45b2dff120220fa9cc55bf5579f47038c90
Jordan Bergero <jbergero@squareup.com>
2023-05-19 10:46:37 -0700

    set up redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [preact()]
 });
---
+++ src/pages/callback.js
@@ -0,0 +1,3 @@
+export async function get({ redirect }) {
+    return redirect('/openapp', 307);
+}
