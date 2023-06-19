ee1a9e0e0884b667afc659bc3fdce192e2a480c5
Jordan Bergero <jordan@bergero.dev>
2023-06-18 23:10:15 -0700

    Revert "hopefully this works"
    This reverts commit fb3c5c49eeeab2f9b2594365ddcfbcc22531a304.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
-import preact from "@astrojs/preact";
 
-import netlify from "@astrojs/netlify/functions";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  integrations: [preact()],
-  adapter: netlify()
+  integrations: [preact()]
 });
--- src/pages/callback.js
+++
@@ -1,3 +0,0 @@
-export async function get({ redirect }) {
-    return redirect('https://bergero.dev/openapp', 307);
-}
